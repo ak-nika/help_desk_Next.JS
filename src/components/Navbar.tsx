dde8d243777e4ee595720b7510e54431794d4e25
akingbayi <akingbayiojo@gmail.com>
2024-08-29 16:54:33 +0100

    Logo added and styling - lesson 5

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,17 @@
 import Link from "next/link";
+import Logo from "@/assets";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav>
+      <Image
+        src={Logo}
+        alt={`Nika Help Desk Logo`}
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
       <h1>Nika Help Desk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
