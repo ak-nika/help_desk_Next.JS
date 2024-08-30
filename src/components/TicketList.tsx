c1aeadeb5d216e1d550f5e93b88ad8d03aa4ca10
akingbayi <akingbayiojo@gmail.com>
2024-08-30 15:58:20 +0100

    fetching tickets - lesson 6

---
+++ src/components/TicketList.tsx
@@ -0,0 +1,38 @@
+async function getTickets() {
+  const res = await fetch("http://localhost:4000/tickets", {
+    next: {
+      revalidate: 0, // This removes the data from being served from cache
+    },
+  });
+
+  return res.json();
+}
+
+export default async function TicketList() {
+  const tickets = await getTickets();
+
+  return (
+    <>
+      {tickets.map(
+        (ticket: {
+          id: string;
+          title: string;
+          body: string;
+          priority: string;
+          user_email: string;
+        }) => (
+          <div key={ticket.id} className="card my-5">
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </div>
+        )
+      )}
+      {tickets.length === 0 && (
+        <p className="text-center">There are no open tickets, yay!</p>
+      )}
+    </>
+  );
+}
