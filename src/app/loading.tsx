2691aa4ff8530e283505ab91a69c6c1873419a80
akingbayi <akingbayiojo@gmail.com>
2024-09-02 13:57:37 +0100

    loading page added - lesson 20

---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <main className="text-center">
+      <p className="text-primary">Loading...</p>
+      <p>Hopefully not for too long :)</p>
+    </main>
+  );
+}
